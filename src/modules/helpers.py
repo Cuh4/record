726ea0a28c6cc8e81a482373f5e77abde0d92efa
Cuh4 <roozzguy@gmail.com>
2023-11-22 16:36:00 +0000

    new helper functions

--- src/modules/helpers.py
+++ src/modules/helpers.py
@@ -2,9 +2,27 @@
 # // ------- [Record] Helpers Module
 # // ---------------------------------------------------------------------
 
+# // ---- Imports
+import sys
+import os
+
 # // ---- Functions
 def RGBToHex(r: float|int = 255, g: float|int = 255, b: float|int = 255):
     return "#{:02x}{:02x}{:02x}".format(r, g, b)
 
 def pathSafeName(name: str):
     return name.replace("\\", "").replace("/", "")
+
+def path(relative_path: str): # https://stackoverflow.com/questions/31836104/pyinstaller-and-onefile-how-to-include-an-image-in-the-exe-file
+    try:
+        base_path = sys._MEIPASS # temp folder where assets are stored. this folder is created by pyinstaller
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+def isBuiltApplication() -> bool:
+    try:
+        return sys._MEIPASS != None
+    except Exception:
+        return False
