51753b1d59cdf6455cdce678a15e331f294a6bf9
Cuh4 <roozzguy@gmail.com>
2023-11-22 17:06:48 +0000

    use os.system instead of pyinstaller.__main__

--- src/build.py
+++ src/build.py
@@ -3,7 +3,7 @@
 # // ---------------------------------------------------------------------
 
 # // ---- Imports
-from PyInstaller import __main__ as PyInstaller
+import os
 
 # // ---- Variables
 fileName = "main.py"
@@ -24,21 +24,23 @@ data = [
 ]
 
 # // ---- Functions
-def format(name: str, values: list[str], ending: str = ""):
+def format(name: str, values: list[str], ending: str = "", wrapInQuotes: bool = False):
     new = []
+    quote = "\"" if wrapInQuotes else ""
     
     for value in values:
-        new.append(f"--{name} {value}{ending}")
+        new.append(f"--{name} {quote}{value}{ending}{quote}")
         
     return tuple(new)
 
 # // ---- Main
-newExclusions = format("exclude", exclusions)
-newData = format("add-data", data, ";.")
+newExclusions = format("exclude", exclusions, wrapInQuotes = True)
+newData = format("add-data", data, ";.", True)
 
-PyInstaller.run(
+os.system(
     # appearance
-    [
+    " ".join([
+        "PyInstaller",
         fileName,
         
         "--name",
@@ -56,7 +58,7 @@ PyInstaller.run(
         
         # data
         *newData
-    ]
+    ])
 )
 
 #py -m PyInstaller main.py --name "Record" --icon "assets/imgs/favicon.png" --onefile --noconsole --add-data "assets/fonts/Montserrat-Black.ttf;." --add-data "assets/fonts/Montserrat-Bold.ttf;." --add-data "assets/fonts/Montserrat-Regular.ttf;." --exclude websockets --exclude pandas --exclude jinja --exclude PIL
